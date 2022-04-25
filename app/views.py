6695e76db708a80688d08fe0fd9a4d9b09910846
Bruno Perez <bueurro@gmail.com>
2022-04-25 19:09:42 -0400

    pagina con static y bloques

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Indexs / login
 
 def index(request):
     return render(request,'app/index.html')
@@ -10,3 +10,41 @@ def login(request):
 
 def homelogin(request):
     return render(request,'app/homelogin.html')
+
+
+#Navbar
+
+def bandanas(request):
+    return render(request,'app/bandanas.html')
+
+def bandanasnl(request):
+    return render(request,'app/bandanasnl.html')
+
+def comidas(request):
+    return render(request,'app/comidas.html')
+
+def comidasnl(request):
+    return render(request,'app/comidasnl.html')
+
+def correas(request):
+    return render(request,'app/correas.html')
+
+def correasnl(request):
+    return render(request,'app/correasnl.html')
+
+def identificaciones(request):
+    return render(request,'app/identificaciones.html')
+
+def identificacionesnl(request):
+    return render(request,'app/identificacionesnl.html')
+
+#logOptions
+
+def carrito(request):
+    return render(request,'app/carrito.html')
+
+def historial(request):
+    return render(request,'app/historial.html')
+
+def suscripcion(request):
+    return render(request,'app/suscripcion.html')
